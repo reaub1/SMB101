25e07b6b59a46327f6204a70568ff9c4de01cb5c
Robin Bechlem <rbechlem@unistra.fr>
2023-03-29 12:36:16 +0200

    Modification du ls.c, il reste à afficher la taille en octet et l'heure de modif

--- TP1/ls.c
+++ TP1/ls.c
@@ -1,35 +1,78 @@
-#include<grp.h>
-#include<pwd.h>
-#include<sys/types.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<dirent.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <string.h>
+#include <pwd.h>
+#include <grp.h>
 
-//main function
-int main(int argc, char *argv[])
+#define STDOUT 1
+#define MAX_PATH_LENGTH 4096
+ 
+
+void display(const char* name)
 {
-
-    //check the number of arguments
-    if (argc != 2)
+    struct stat st;
+    if(stat(name, &st)== -1)
     {
-        printf("Error on number of arguments\n");
-        printf("Usage: %s <Path>\n",argv[1]);
+        perror("stat");
         exit(1);
     }
 
-    //show the file of the path
+
+    struct passwd *pw = getpwuid(st.st_uid);
+
+    struct group *gr = getgrgid(st.st_gid);
+
+    char mode[9]="---------";
+
+    if( st.st_mode & S_IRUSR )
+        //printf("r");
+        mode[0]='r';
+    if( st.st_mode & S_IWUSR )
+        //printf("w");
+        mode[1]='w';
+    if( st.st_mode & S_IXUSR )
+        //printf("x");
+        mode[2]='x';
+    if( st.st_mode & S_IRGRP )
+        //printf("r");
+        mode[3]='r';
+    if( st.st_mode & S_IWGRP )
+        //printf("w");
+        mode[4]='w';
+    if( st.st_mode & S_IXGRP )
+        //printf("x");
+        mode[5]='x';
+    if( st.st_mode & S_IROTH )
+        //printf("r");
+        mode[6]='r';
+    if( st.st_mode & S_IWOTH )
+        //printf("w");
+        mode[7]='w';
+    if( st.st_mode & S_IXOTH )
+        //printf("x");
+        mode[8]='x';
+
+    printf("%s -  %s - %s:%s\n", name,mode, pw->pw_name, gr->gr_name);
+} 
+int main(int argc, char** argv)
+{
+    DIR *d;
     struct dirent *dir;
-
-    DIR *d = opendir(argv[1]);
-
-    if(d)
-    {
-        while(dir=readdir(d))
-        {
-            printf("%s\n",dir->d_name);
+    d = opendir(getenv("PWD"));
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            display(dir->d_name);
         }
+        closedir(d);
     }
-
-
-    return 0;
+    return(0);
 }
